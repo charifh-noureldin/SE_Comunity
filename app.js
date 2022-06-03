f59549e566394b113ad3b9ebed8cb11d075b35c6
nech <nech@MBP-von-nech.fritz.box>
2022-06-03 23:01:30 +0200

    add questions page and api page

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
+app.get("/questions", (req, res) => {
+  res.render("questions", { title: "Questions" });
+});
 
 app.get("/index", (req, res) => {
   res.render("index", { title: "Home" });
